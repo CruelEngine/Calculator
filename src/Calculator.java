80d354089abe87797519dfa7b2913b9b17bdc2bb
CruelEngine <babhineethbhat@gmail.com>
2022-05-24 23:05:19 +0530

    Refactored code

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
             return 0;
         }else {
             String[] numberList = numbers.split(","); // split the string using ',' as separator
-            if(numberList.length == 1) {
+            if(numberList.length < 2) { // why less than 2? just to support length == 1
                 return Integer.valueOf(numberList[0]);
             }else {
                 return Integer.valueOf(numberList[0]) + Integer.valueOf(numberList[1]);
