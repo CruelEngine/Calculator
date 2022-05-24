6302af2a8671904f111c3df8115b69fadecd42af
CruelEngine <babhineethbhat@gmail.com>
2022-05-24 23:55:11 +0530

    Should add unlimited numbers in the string separated by comma: Pass

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,7 +7,11 @@ public class Calculator {
         if(numberList.length < 2) { // why less than 2? just to support length == 1
             return Integer.valueOf(numberList[0]);
         }else {
-            return Integer.valueOf(numberList[0]) + Integer.valueOf(numberList[1]);
+            int sum = 0;
+            for(int i =0 ; i < numberList.length; i++) {
+                sum += Integer.valueOf(numberList[i]);
+            }
+            return sum;
         }
     }
 }
