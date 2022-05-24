a67fff6ea8fafbdd7cd336ef46566f5b8633f646
CruelEngine <babhineethbhat@gmail.com>
2022-05-24 22:39:26 +0530

    Added Impl for the add function to return 0 if the string is empty

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,8 @@
 public class Calculator {
-    public int add(String number){}
+    public int add(String numbers){
+        if(numbers.isEmpty()){
+            return 0;
+        }
+        return 0;
+    }
 }
