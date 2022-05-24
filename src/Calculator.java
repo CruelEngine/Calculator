c3ef318debb905c91ae6684e7ada5491565eef33
CruelEngine <babhineethbhat@gmail.com>
2022-05-24 22:56:35 +0530

    Added Production Code to support single number in string

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,11 @@ public class Calculator {
     public int add(String numbers){
         if(numbers.isEmpty()){
             return 0;
+        }else {
+            String[] numberList = numbers.split(","); // split the string using ',' as separator
+            if(numberList.length == 1) {
+                return Integer.valueOf(numberList[0]);
+            }
         }
         return 0;
     }
