dd63092b285279374dd88c33ac53ce72be3aa059
CruelEngine <babhineethbhat@gmail.com>
2022-05-25 00:02:50 +0530

    Code refactored and all test cases pass

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,14 +4,10 @@ public class Calculator {
             return 0;
         }
         String[] numberList = numbers.split(","); // split the string using ',' as separator
-        if(numberList.length < 2) { // why less than 2? just to support length == 1
-            return Integer.valueOf(numberList[0]);
-        }else {
-            int sum = 0;
-            for(int i =0 ; i < numberList.length; i++) {
-                sum += Integer.valueOf(numberList[i]);
-            }
-            return sum;
+        int sum = 0;
+        for(int i =0 ; i < numberList.length; i++) {
+            sum += Integer.valueOf(numberList[i]);
         }
+        return sum;
     }
 }
