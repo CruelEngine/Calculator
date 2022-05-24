0ff99732e852906b3eb2d69feb403463a90cb754
CruelEngine <babhineethbhat@gmail.com>
2022-05-24 22:26:58 +0530

    test for empty string

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,3 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculatorTest {
     private Calculator calculator;
+
+    @Test
+    private void addTest() {
+        Assert.assertEquals("should return 0 for empty string", calculator.add(""));
+    }
 }
