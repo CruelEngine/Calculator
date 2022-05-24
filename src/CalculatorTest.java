6970fe6ec8eb521ab820042ed07b8deaa612170d
CruelEngine <babhineethbhat@gmail.com>
2022-05-24 22:53:25 +0530

    Added Test to find the sum of string with one number

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -8,5 +8,6 @@ public class CalculatorTest {
     public void addTest() {
         calculator = new Calculator();
         Assert.assertEquals(0, calculator.add(""));
+        Assert.assertEquals(1, calculator.add("1"));
     }
 }
