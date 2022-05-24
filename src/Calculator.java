4cc6f260b2b0d4a70123baf4be54d4a4ff592741
CruelEngine <babhineethbhat@gmail.com>
2022-05-24 23:12:50 +0530

    Refactored code and confirmed all the assertion passes

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,13 +2,12 @@ public class Calculator {
     public int add(String numbers){
         if(numbers.isEmpty()){
             return 0;
+        }
+        String[] numberList = numbers.split(","); // split the string using ',' as separator
+        if(numberList.length < 2) { // why less than 2? just to support length == 1
+            return Integer.valueOf(numberList[0]);
         }else {
-            String[] numberList = numbers.split(","); // split the string using ',' as separator
-            if(numberList.length < 2) { // why less than 2? just to support length == 1
-                return Integer.valueOf(numberList[0]);
-            }else {
-                return Integer.valueOf(numberList[0]) + Integer.valueOf(numberList[1]);
-            }
+            return Integer.valueOf(numberList[0]) + Integer.valueOf(numberList[1]);
         }
     }
 }
