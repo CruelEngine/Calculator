a68b24ffd9a8d1376c4e07b8b8c3400c6846a174
CruelEngine <babhineethbhat@gmail.com>
2022-05-25 22:59:28 +0530

    Add list of numbers using new line as separator: Passed

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,6 +3,8 @@ public class Calculator {
         if(numbers.isEmpty()){
             return 0;
         }
+        // replace newline with comma
+        numbers = numbers.replace("\n", ",");
         String[] numberList = numbers.split(","); // split the string using ',' as separator
         int sum = 0;
         for(int i =0 ; i < numberList.length; i++) {
