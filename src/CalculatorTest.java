495785b3ed8c88a32dbefb733b28e88cde7c3739
CruelEngine <babhineethbhat@gmail.com>
2022-05-26 02:14:14 +0530

    Moved each asset into its own test case

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -15,26 +15,75 @@ public class CalculatorTest {
     }
 
     @Test
-    public void addTest() {
+    public void shouldDoNothing() {
+
+    }
+
+    @Test
+    public void testEmptyString() {
         assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    public void testSingleNumber() {
         assertEquals(1, calculator.add("1"));
+    }
+
+    @Test
+    public void testAdditionOfTwoNumbers() {
         assertEquals(3, calculator.add("1,2"));
         assertEquals(15, calculator.add("8,7"));
-        assertEquals(40, calculator.add("5,6,7,9,13"));
-        assertEquals(6,calculator.add("1\n2,3"));
-        assertEquals(3, calculator.add("//;\n1;2"));
+    }
 
+    @Test
+    public void testAdditionOfUnlimitedNumbers() {
+        assertEquals(40, calculator.add("5,6,7,9,13"));
+    }
+
+    @Test
+    public void testAdditionUsingNewlineDelimiter() {
+        assertEquals(6,calculator.add("1\n2,3"));
+    }
+
+    @Test
+    public void testAdditionUsingCustomDelimiter() {
+        assertEquals(3, calculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public void testNegativeNumbersNotAllowed() {
         try {
             calculator.add("-1,2");
             fail("Exception not thrown");
         }catch(IllegalArgumentException exception) {
             assertEquals("negatives not allowed", exception.getMessage());
         }
+    }
 
+    @Test
+    public void testNumbersGreaterThan1000LimitNotEvaluated() {
         assertEquals(2, calculator.add("1001,2"));
+        assertEquals(2, calculator.add("2001,2"));
+    }
+
+    @Test
+    public void testMultipleCharactersOfSameDelimiter() {
         assertEquals(10, calculator.add("//[***]\n2***3***5"));
+    }
+
+    @Test
+    public void testMultipleDelimiter() {
         assertEquals(6, calculator.add("//[*][%]\n1*2%3"));
+    }
+
+    @Test
+    public void testMultipleCharacterSequenceDelimiter() {
         assertEquals(6, calculator.add("//[*][%]\n1**2%%3"));
+    }
+
+    @Test
+    public void testMultipleCombinationOfDelimiter() {
         assertEquals(6, calculator.add("//[*][%]\n1*%2%*3"));
     }
+
 }
