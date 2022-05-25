6f1791ca10bd36e2f24d544e817fc7295a31de29
CruelEngine <babhineethbhat@gmail.com>
2022-05-25 23:51:03 +0530

    Test sum of numbers separated by cutom delimitted: Pass

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,27 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
     public int add(String numbers){
         if(numbers.isEmpty()){
             return 0;
         }
+        String delimitterPattern = "\\/\\/(.*?)\\n";
+        Pattern pattern = Pattern.compile(delimitterPattern);
+        Matcher patternMatcher = pattern.matcher(numbers);
+        String delimitter=  "\n";
+        if(patternMatcher.find()) {
+            System.out.print(patternMatcher.group(0));
+            String delimitterBoundary = patternMatcher.group(0);
+            System.out.print(patternMatcher.group(1));
+            delimitter = patternMatcher.group(1);
+            numbers = numbers.replace(delimitterBoundary, "");
+            System.out.println(numbers);
+        }
+
         // replace newline with comma
-        numbers = numbers.replace("\n", ",");
+        numbers = numbers.replace(delimitter, ",");
+        System.out.println(numbers);
         String[] numberList = numbers.split(","); // split the string using ',' as separator
         int sum = 0;
         for(int i =0 ; i < numberList.length; i++) {
