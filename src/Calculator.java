6ab6f894524d7de4768d25970c7bc079266205d4
CruelEngine <babhineethbhat@gmail.com>
2022-05-26 01:15:15 +0530

    Allow multiple delimiters: Pass

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,17 +13,17 @@ public class Calculator {
         Matcher patternMatcher = pattern.matcher(numbers);
         String delimitter=  "\n";
         if(patternMatcher.find()) {
-            System.out.print(patternMatcher.group(0));
             String delimitterBoundary = patternMatcher.group(0);
-            System.out.print(patternMatcher.group(1));
             delimitter = patternMatcher.group(1);
             numbers = numbers.replace(delimitterBoundary, "");
-            System.out.println(numbers);
         }
-
-        // replace newline with comma
         numbers = numbers.replace(delimitter, ",");
-        System.out.println(numbers);
+        if(delimitter.length() > 1) {
+            String[] delimiterList = delimitter.split("");
+            for(int j  =0 ; j < delimiterList.length ;j++) {
+                numbers = numbers.replace(delimiterList[j], ",");
+            }
+        }
         String[] numberList = numbers.split(","); // split the string using ',' as separator
         int sum = 0;
         for(int i =0 ; i < numberList.length; i++) {
